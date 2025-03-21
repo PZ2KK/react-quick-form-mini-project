53896d9ae30ad0451d31fad303bcf06edd3b0f5c
Pom <kasiphit@gmail.com>
2025-03-21 09:37:01 +0700

    Set up project structure

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import MovieSurvey from './components/movieSurvey'
+
+function App() {
+  return (
+    <>
+      <MovieSurvey />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/movieSurvey.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react"
+import movies from "../data/moviesData"
+import { TbMovie } from "react-icons/tb";
+
+const MovieSurvey = () => {
+    return (
+        // Header
+        <div>
+            <h1><TbMovie color="white" size="30px"/> Movie Survey</h1>
+        </div>
+
+        
+    );
+};
+
+export default MovieSurvey;
